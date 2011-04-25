7a35c67225f8c0ec960f3beb53f171cc29522082
Marlena Compton <mozmarlena@gmail.com>
2011-04-25 14:52:22 -0700

    test searching for fire

--- test_api.py
+++ test_api.py
@@ -5,7 +5,7 @@
 
 from BeautifulSoup import BeautifulStoneSoup
 #from selenium import selenium
-#from vars import ConnectionParameters
+from vars import ConnectionParameters
 import unittest
 import urllib2
 #import pytest
@@ -13,19 +13,25 @@ import urllib2
 
 
 
-class APITests(unittest.TestCase):
-
-
+class AddOnsAPI(object):
 	
+	
+	_api_base_url = ConnectionParameters.baseurl + '/en-us/firefox/api/1.5/search'	
+	
+	_firebug_url = _api_base_url + '/firebug'		
+		
+	def __init__(self,api_search_term):
+		pass
+			
+class APITests(unittest.TestCase):		
 
-		
-	def test_api(self):  #test_that_firebug_is_listed_first_in_api_search_results_for_fire(self):
+	def test_check_that_firebug_is_listed_first_on_searching_for_fire(self):  
 		""" TestCase for Litmus 15314 """
-		xml_soup = BeautifulStoneSoup(urllib2.urlopen('https://addons.allizom.org/en-us/firefox/api/1.5/search/fire'))
+		url = urllib2.urlopen(AddOnsAPI._api_base_url + '/fire')
+		xml_soup = BeautifulStoneSoup(url)
 		name_of_first_addon_listed = xml_soup.searchresults.addon.nameTag.contents[0]
-		self.assertEquals("Firebug", name_of_first_addon_listed)
-		
-		
+		self.assertEquals("Firebug", name_of_first_addon_listed)		
+					
 if __name__ == "__main__":
 	unittest.main()
 
