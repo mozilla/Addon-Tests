cd4c7f1ca45924779ea833f55d9550b19a4e12b6
Marlena Compton <mozmarlena@gmail.com>
2011-04-25 18:17:02 -0700

    changed name of method to get name of first addon

--- test_api.py
+++ test_api.py
@@ -19,7 +19,7 @@ class AddOnsAPI(object):
         self.search_url = self.api_base_url + search_extension      
         self.parsed_xml = BeautifulStoneSoup(urllib2.urlopen(self.search_url) )
         
-    def get_name_of_nth_addon(self, n):
+    def get_name_of_first_addon(self):
         name_of_first_addon_listed = self.parsed_xml.searchresults.addon.nameTag.contents[0]
         return name_of_first_addon_listed
             
@@ -40,12 +40,12 @@ class APITests(unittest.TestCase):
     def test_check_that_firebug_is_listed_first_in_addons_search_for_fire(self):  
         """ TestCase for Litmus 15314 """       
         addons_xml = AddOnsAPI('fire')
-        self.assertEquals("Firebug", addons_xml.get_name_of_nth_addon(1))
+        self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
 
     def test_check_that_firebug_is_listed_first_in_addons_search_for_firebug(self):  
         """ TestCase for Litmus 15316 """
         addons_xml = AddOnsAPI()
-        self.assertEquals("Firebug", addons_xml.get_name_of_nth_addon(1))
+        self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
                     
 if __name__ == "__main__":
     unittest.main()
