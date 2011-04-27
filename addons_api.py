470903a9d5b7197c955aaca076b239c3095b79f3
Marlena Compton <mozmarlena@gmail.com>
2011-04-27 16:58:30 -0700

    added tests & methods for addons version and extension type

--- addons_api.py
+++ addons_api.py
@@ -15,3 +15,15 @@ class AddOnsAPI(object):
     def get_name_of_first_addon(self):
         name_of_first_addon_listed = self.parsed_xml.searchresults.addon.nameTag.contents[0]
         return name_of_first_addon_listed
+
+	def get_addon_type_name(self):
+		addon_type_name = self.parsed_xml.addon.type.string
+		return addon_type_name
+		
+	def get_addon_type_id(self):
+		addon_type_id = self.parsed_xml.addon.type["id"]
+		return addon_type_id
+		
+	def get_addon_version_number(self):
+		addon_version = self.parsed_xml.addon.version.string
+		return addon_version_number
--- test_api_only.py
+++ test_api_only.py
@@ -8,15 +8,31 @@ import unittest2 as unittest
 #These tests should only call the api.  There should be no tests requiring selenium in this class.
 class APIOnlyTests(unittest.TestCase):
 
-    def test_check_that_firebug_is_listed_first_in_addons_search_for_fire(self):  
+    def test_that_firebug_is_listed_first_in_addons_search_for_fire(self):  
         """ TestCase for Litmus 15314 """       
         addons_xml = AddOnsAPI('fire')
         self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
 
-    def test_check_that_firebug_is_listed_first_in_addons_search_for_firebug(self):  
+    def test_that_firebug_is_listed_first_in_addons_search_for_firebug(self):  
         """ TestCase for Litmus 15316 """
         addons_xml = AddOnsAPI()
         self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
+
+	def test_that_firebug_addon_type_name_is_extension(self):
+		"Testcase for Litmus 15316"
+		addons_xml = AddOnsAPI()
+		self.assertEquals("Extension", addons_xml.get_addon_type_name())
+	
+	def test_that_firebug_addon_type_id_is_1(self):
+		"Testcase for Litmus 15316"
+		addon_xml = AddonsAPI()
+		self.assertEquals("1", addons_xml.get_addon_type_id())
+	
+	def test_firebug_version_number(self):
+		"Testcase for Litmus 15317"
+		addon_xml = AddonsAPI()
+		print addon_xml.get_addon_version_number()
+		self.assertEquals("1.7.0", addons_xml.get_addon_version_number())
                     
 if __name__ == "__main__":
     unittest.main()
