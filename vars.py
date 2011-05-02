e1b48d1e57ba6d54a0457b35fe08824f3de804c9
Marlena Compton <mozmarlena@gmail.com>
2011-05-02 13:39:43 -0700

    updated browser to Firefox-4

--- vars.py
+++ vars.py
@@ -39,8 +39,8 @@
 class ConnectionParameters:
     server = "localhost"
     port = 4444
-    browser = "Firefox4b;en-us;MacOSX6"
-    baseurl = "http://addons.allizom.org"
+    browser = "Firefox-4;en-us;MacOSX6"
+    baseurl = "http://addons.allizom.org" #staging
     page_load_timeout = 120000
 
 
