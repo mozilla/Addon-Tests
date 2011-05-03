986939bfeebcb239048ccf8aee1434b8b24e107f
Marlena Compton <mozmarlena@gmail.com>
2011-05-03 11:50:28 -0700

    added error handling and tests for raising errors

--- addons_api.py
+++ addons_api.py
@@ -10,25 +10,42 @@ class AddOnsAPI(object):
         self.api_base_url = ConnectionParameters.baseurl + '/en-us/firefox/api/1.5/search/'
         self.search_url = self.api_base_url + search_extension      
         self.parsed_xml = BeautifulStoneSoup(urllib2.urlopen(self.search_url) )
-        
+                        
     def get_xml_for_single_addon(self, addon_name):
-        addon_xml = self.parsed_xml.find(text=addon_name).findParent().findParent()
-        return addon_xml
+        try:
+            addon_xml = self.parsed_xml.find(text=addon_name).findParent().findParent()
+            return addon_xml
+        except AttributeError:
+            self.print_search_error()
         
     def get_name_of_first_addon(self):
-        name_of_first_addon_listed = self.parsed_xml.searchresults.addon.nameTag.contents[0]
-        return name_of_first_addon_listed
+        try:
+            name_of_first_addon_listed = self.parsed_xml.searchresults.addon.nameTag.contents[0]
+            return name_of_first_addon_listed
+        except:
+            print 'Check that searchresults returned valid xml'
 
     def get_addon_type_name(self, addon_name):
-        addon_xml =  self.get_xml_for_single_addon(addon_name)  
-        return addon_xml.type.string        
-        
-        
+        try:
+            addon_xml =  self.get_xml_for_single_addon(addon_name)  
+            return addon_xml.type.string
+        except:                
+            self.print_search_error()
+            
     def get_addon_type_id(self, addon_name):
-        addon_xml = self.get_xml_for_single_addon(addon_name)
-        return addon_xml.type["id"]
+        try:
+            addon_xml = self.get_xml_for_single_addon(addon_name)
+            return addon_xml.type["id"]
+        except AttributeError:
+            self.print_search_error()
         
     def get_addon_version_number(self, addon_name):
-        addon_xml = self.get_xml_for_single_addon(addon_name)
-        return addon_xml.version.string
+        try:
+            addon_xml = self.get_xml_for_single_addon(addon_name)
+            return addon_xml.version.string
+        except AttributeError:
+            self.print_search_error()
+        
+    def print_search_error(self):
+        print('The addon is not in the search results.')
         
--- test_api_infrastructure.py
+++ test_api_infrastructure.py
@@ -7,14 +7,29 @@ import unittest2 as unittest
 
 #These tests should only call the api.  There should be no tests requiring selenium in this class.
 class APIOnlyTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.addons_xml = AddOnsAPI()
         
     def test_get_addon_type_name(self):
-        addons_xml = AddOnsAPI()
-        self.assertEquals("Extension", addons_xml.get_addon_type_name("Firebug"))#("Firefox 3 theme for Firefox 4"))
+        self.assertEquals("Extension", self.addons_xml.get_addon_type_name("Firebug"))#("Firefox 3 theme for Firefox 4"))
         
-    def test_get_firefinder_version_number(self):
-        addons_xml = AddOnsAPI()
-        self.assertEquals("1.1.6", addons_xml.get_addon_version_number("Illuminations for Developers for Firebug"))
-                    
+    def test_get_version_number(self):
+        self.assertEquals("1.1.6", self.addons_xml.get_addon_version_number("Illuminations for Developers for Firebug"))
+                
+    def test_addon_is_not_in_search_results(self):
+        self.assertRaises(AttributeError, self.addons_xml.get_xml_for_single_addon("Firepug"))
+    
+    def test_first_addon_fails_when_addon_is_not_in_results(self):
+        bad_xml = AddOnsAPI("Firepug")
+        self.assertRaises(AttributeError, bad_xml.get_name_of_first_addon())
+    
+    def test_addon_type_fails_when_addon_is_not_in_results(self):
+        self.assertRaises(AttributeError, self.addons_xml.get_addon_type_id("Firepug"))
+        self.assertRaises(AttributeError, self.addons_xml.get_addon_type_name("Firepug"))
+    
+    def test_addon_version_number_fails_when_addon_is_not_in_results(self):
+        self.assertRaises(AttributeError, self.addons_xml.get_addon_version_number("Firepug"))
+             
 if __name__ == "__main__":
     unittest.main()
