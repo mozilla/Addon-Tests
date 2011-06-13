614f786ca0b0a0469f4e069f219ec1bdeaf4740e
Marlena Compton <mozmarlena@gmail.com>
2011-06-13 16:53:53 -0700

    change api tests to refect shift to conftest instead of vars

--- test_api_only.py
+++ test_api_only.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python
 
 from addons_api import AddOnsAPI
-import unittest2 as unittest
-#import pytest
-#xfail = pytest.mark.xfail
+from unittestzero import Assert
+import pytest
 
 #These tests should only call the api.  There should be no tests requiring selenium in this class.
-class APIOnlyTests(unittest.TestCase):
+class APIOnlyTests:
 
-    def test_that_firebug_is_listed_first_in_addons_search_for_fire(self):  
+    def test_that_firebug_is_listed_first_in_addons_search_for_fire(self, testsetup):  
         """TestCase for Litmus 15314"""      
         addons_xml = AddOnsAPI('fire')
-        self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
+        Assert.equal("Firebug", addons_xml.get_name_of_first_addon())
 
-    def test_that_firebug_is_listed_first_in_addons_search_for_firebug(self):  
+    def test_that_firebug_is_listed_first_in_addons_search_for_firebug(self, testsetup):  
         """TestCase for Litmus 15316"""
         addons_xml = AddOnsAPI()
-        self.assertEquals("Firebug", addons_xml.get_name_of_first_addon())
+        Assert.equal("Firebug", addons_xml.get_name_of_first_addon())
 
-    def test_that_firebug_addon_type_name_is_extension(self):
+    def test_that_firebug_addon_type_name_is_extension(self, testsetup):
         """Testcase for Litmus 15316"""
         addons_xml = AddOnsAPI()
-        self.assertEquals("Extension", addons_xml.get_addon_type_name("Firebug"))
+        Assert.equal("Extension", addons_xml.get_addon_type_name("Firebug"))
     
-    def test_that_firebug_addon_type_id_is_1(self):
+    def test_that_firebug_addon_type_id_is_1(self, testsetup):
         """Testcase for Litmus 15316"""
         addon_xml = AddOnsAPI()
-        self.assertEquals("1", addon_xml.get_addon_type_id("Firebug"))
+        Assert.equal("1", addon_xml.get_addon_type_id("Firebug"))
     
-    def test_firebug_version_number(self):
+    def test_firebug_version_number(self, testsetup):
         """Testcase for Litmus 15317"""
         addon_xml = AddOnsAPI()
-        self.assertEquals("1.7.0", addon_xml.get_addon_version_number("Firebug"))
+        Assert.equal("1.7.0", addon_xml.get_addon_version_number("Firebug"))
                     
 if __name__ == "__main__":
     unittest.main()
