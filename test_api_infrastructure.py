a74d03965e669adf8a69e797efd29819654f3641
Marlena Compton <mozmarlena@gmail.com>
2011-06-16 11:57:27 -0700

    fixing debug tests waiting for Assert.raises

--- test_api_infrastructure.py
+++ test_api_infrastructure.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python
 
 from addons_api import AddOnsAPI
-import unittest2 as unittest
-#import pytest
-#xfail = pytest.mark.xfail
+from unittestzero import Assert
+import pytest
 
 #These tests should only call the api.  There should be no tests requiring selenium in this class.
-class APIOnlyTests(unittest.TestCase):
-    
-    def setUp(self):
-        self.addons_xml = AddOnsAPI()
+class TestAPIOnlyTests():
+            
+    @pytest.mark.skip_selenium  
+    def test_get_addon_type_name(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
+        Assert.equal("Extension", addons_xml.get_addon_type_name("Firebug"))#("Firefox 3 theme for Firefox 4"))
         
-    def test_get_addon_type_name(self):
-        self.assertEquals("Extension", self.addons_xml.get_addon_type_name("Firebug"))#("Firefox 3 theme for Firefox 4"))
-        
-    def test_get_version_number(self):
-        self.assertEquals("1.1.6", self.addons_xml.get_addon_version_number("Illuminations for Developers for Firebug"))
+    @pytest.mark.skip_selenium
+    def test_get_version_number(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
+        Assert.equal("1.1.6", addons_xml.get_addon_version_number("Illuminations for Developers for Firebug"))
                 
-    def test_addon_is_not_in_search_results(self):
-        self.assertRaises(AttributeError, self.addons_xml.get_xml_for_single_addon("Firepug"))
+    @pytest.mark.skip_selenium
+    def test_addon_is_not_in_search_results(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
+        Assert.raises(AttributeError, addons_xml.get_xml_for_single_addon("Firepug"))
     
-    def test_first_addon_fails_when_addon_is_not_in_results(self):
+    @pytest.mark.skip_selenium
+    def test_first_addon_fails_when_addon_is_not_in_results(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
         bad_xml = AddOnsAPI("Firepug")
-        self.assertRaises(AttributeError, bad_xml.get_name_of_first_addon())
+        Assert.raises(AttributeError, bad_xml.get_name_of_first_addon())
     
-    def test_addon_type_fails_when_addon_is_not_in_results(self):
-        self.assertRaises(AttributeError, self.addons_xml.get_addon_type_id("Firepug"))
-        self.assertRaises(AttributeError, self.addons_xml.get_addon_type_name("Firepug"))
+    @pytest.mark.skip_selenium
+    def test_addon_type_fails_when_addon_is_not_in_results(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
+        Assert.raises(AttributeError, addons_xml.get_addon_type_id("Firepug"))
+        Assert.raises(AttributeError, addons_xml.get_addon_type_name("Firepug"))
     
-    def test_addon_version_number_fails_when_addon_is_not_in_results(self):
-        self.assertRaises(AttributeError, self.addons_xml.get_addon_version_number("Firepug"))
+    @pytest.mark.skip_selenium
+    def test_addon_version_number_fails_when_addon_is_not_in_results(self, testsetup):
+        addons_xml = AddOnsAPI(testsetup)
+        Assert.raises(AttributeError, addons_xml.get_addon_version_number("Firepug"))
              
 if __name__ == "__main__":
     unittest.main()
