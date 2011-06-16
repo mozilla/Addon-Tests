825575bed5192f2b4e0559c0400e0fba0705910a
Marlena Compton <mozmarlena@gmail.com>
2011-06-15 17:48:30 -0700

    remove comment

--- addons_api.py
+++ addons_api.py
@@ -10,7 +10,6 @@ class AddOnsAPI(object):
     def __init__(self, testsetup, search_extension = 'firebug'):
         
         self.api_base_url = testsetup.base_url + '/en-us/firefox/api/1.5/search/'
-        #self.api_base_url = ConnectionParameters.baseurl + '/en-us/firefox/api/1.5/search/'
         self.search_url = self.api_base_url + search_extension      
         self.parsed_xml = BeautifulStoneSoup(urllib2.urlopen(self.search_url) )
         
