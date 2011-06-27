b18aeefa23ef24b61b071acf983096767b3ca6bb
Marlena Compton <mozmarlena@gmail.com>
2011-06-27 14:44:05 -0700

    pep8 cleanup

--- addons_api.py
+++ addons_api.py
@@ -13,14 +13,14 @@ class AddOnsAPI(object):
         self.api_base_url = testsetup.base_url + '/en-us/firefox/api/1.5/search/'
         self.search_url = self.api_base_url + search_extension
         self.parsed_xml = BeautifulStoneSoup(urllib2.urlopen(self.search_url))
- 
+
     def get_xml_for_single_addon(self, addon_name):
         try:
             addon_xml = self.parsed_xml.find(text=addon_name).findParent().findParent()
             return addon_xml
         except AttributeError:
             self._print_search_error()
-        
+
     def get_name_of_first_addon(self):
         try:
             name_of_first_addon_listed = self.parsed_xml.searchresults.addon.nameTag.contents[0]
@@ -32,9 +32,9 @@ class AddOnsAPI(object):
         try:
             addon_xml = self.get_xml_for_single_addon(addon_name)
             return addon_xml.type.string
-        except:                
+        except:
             self._print_search_error()
-            
+
     def get_addon_type_id(self, addon_name):
         try:
             addon_xml = self.get_xml_for_single_addon(addon_name)
@@ -53,10 +53,10 @@ class AddOnsAPI(object):
         try:
             addon_xml = self.get_xml_for_single_addon(addon_name)
             name_tags = addon_xml.authors.findAll('name')
-                        
+
             return [ BeautifulStoneSoup(str(name_tags[i])).find('name').string
                 for i in range(len(name_tags)) ]
-        
+
         except AttributeError:
             self._print_search_error()
         
