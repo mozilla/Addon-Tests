c937dda028768e9ac3870719b84eff7cefc0fcdc
Marlena Compton <mozmarlena@gmail.com>
2011-07-05 16:41:02 -0700

    cleaned up debug code

--- test_details_page_against_xml.py
+++ test_details_page_against_xml.py
@@ -59,25 +59,16 @@ class TestDetailsPageAgainstXML:
         xml_description = addons_xml.get_addon_description(self.firebug)
 
         Assert.equal(browser_description, xml_description)
-        
+
     def test_that_icon_is_correct(self, testsetup):
-        """litmus """
-        
+        """litmus 15322"""
+
         #browser
         firebug_page = AddonsDetailsPage(testsetup, self.firebug)
-        Assert.true(firebug_page.is_element_present("css=img[class='icon']"))
-        
-        
-        
-        #try get_attribute(locator) to see if that will return the html string for icon locator
-        #javadoc:
-        #http://release.seleniumhq.org/selenium-remote-control/0.9.2/doc/java/com/thoughtworks/selenium/Selenium.html
-        
+        browser_icon = firebug_page.icon_url
+
         #xml
         addons_xml = AddOnsAPI(testsetup)
-        xml_icon_url = addons_xml.get_icon_url(self.firebug)
-        
-        Assert.equal( firebug_page.selenium.get_attribute("css=img[class='icon']@src"), xml_icon_url )
-        
-        the_url = 'https://gs1.adn.edgecastcdn.net/801237/addons-cdn.allizom.org/images/addon_icon/1843-32.png?modified=1308640553'
-        Assert.equal('https://gs1.adn.edgecastcdn.net/801237/addons-cdn.allizom.org/images/addon_icon/1843-32.png?modified=1308640553', xml_icon_url)
+        xml_icon = addons_xml.get_icon_url(self.firebug)
+
+        Assert.equal(browser_icon, xml_icon)
