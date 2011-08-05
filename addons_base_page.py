782af66d0e0923b885e5c0bedada0bde334b85e2
Alex Rodionov <p0deje@gmail.com>
2011-08-05 16:24:06 +0700

    Updated Contributors section - thanks @bebe1987

--- addons_base_page.py
+++ addons_base_page.py
@@ -20,6 +20,7 @@
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s): Bebe <florin.strugariu@softvision.ro>
+#                 Alex Rodionov <p0deje@gmail.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
