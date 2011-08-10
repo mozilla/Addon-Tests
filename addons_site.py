180d3e12f3f58d4c68b823ecf70f46d2d876523b
Marlena Compton <mozmarlena@gmail.com>
2011-08-10 06:16:50 -0700

    giving the license a good smack

--- addons_site.py
+++ addons_site.py
@@ -31,7 +31,7 @@
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
 # the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case thHehee provisions of the GPL or the LGPL are applicable instead
+# in which case the provisions of the GPL or the LGPL are applicable instead
 # of those above. If you wish to allow use of your version of this file only
 # under the terms of either the GPL or the LGPL, and not to allow others to
 # use your version of this file under the terms of the MPL, indicate your
