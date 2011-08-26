5f398357d702140a6521e2eb4a21b6baaca6406b
Dave Hunt <dave.hunt@gmail.com>
2011-08-26 16:55:36 +0100

    Switch tests over to use the pytest-mozwebqa plugin.

--- test_api_only.py
+++ test_api_only.py
@@ -11,27 +11,27 @@ import pytest
 @pytest.mark.skip_selenium
 class TestAPIOnlyTests:
 
-    def test_that_firebug_is_listed_first_in_addons_search_for_fire(self, testsetup):
+    def test_that_firebug_is_listed_first_in_addons_search_for_fire(self, mozwebqa):
         """TestCase for Litmus 15314"""
-        addons_xml = AddOnsAPI(testsetup, 'fire')
+        addons_xml = AddOnsAPI(mozwebqa, 'fire')
         Assert.equal("Firebug", addons_xml.get_name_of_first_addon())
 
-    def test_that_firebug_is_listed_first_in_addons_search_for_firebug(self, testsetup):
+    def test_that_firebug_is_listed_first_in_addons_search_for_firebug(self, mozwebqa):
         """TestCase for Litmus 15316"""
-        addons_xml = AddOnsAPI(testsetup)
+        addons_xml = AddOnsAPI(mozwebqa)
         Assert.equal("Firebug", addons_xml.get_name_of_first_addon())
 
-    def test_that_firebug_addon_type_name_is_extension(self, testsetup):
+    def test_that_firebug_addon_type_name_is_extension(self, mozwebqa):
         """Testcase for Litmus 15316"""
-        addons_xml = AddOnsAPI(testsetup)
+        addons_xml = AddOnsAPI(mozwebqa)
         Assert.equal("Extension", addons_xml.get_addon_type_name("Firebug"))
 
-    def test_that_firebug_addon_type_id_is_1(self, testsetup):
+    def test_that_firebug_addon_type_id_is_1(self, mozwebqa):
         """Testcase for Litmus 15316"""
-        addon_xml = AddOnsAPI(testsetup)
+        addon_xml = AddOnsAPI(mozwebqa)
         Assert.equal("1", addon_xml.get_addon_type_id("Firebug"))
 
-    def test_firebug_version_number(self, testsetup):
+    def test_firebug_version_number(self, mozwebqa):
         """Testcase for Litmus 15317"""
-        addon_xml = AddOnsAPI(testsetup)
+        addon_xml = AddOnsAPI(mozwebqa)
         Assert.equal("1.8.0", addon_xml.get_addon_version_number("Firebug"))
