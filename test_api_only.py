ead424f1eb71597f16bc63a05a41bf88e6255cea
Stephen Donner <stephen.donner@gmail.com>
2011-08-30 14:16:51 -0700

    Revving the version # of Firebug to be 1.8.1, from 1.8.0.  See https://addons-dev.allizom.org/en-US/firefox/addon/firebug/

--- test_api_only.py
+++ test_api_only.py
@@ -34,4 +34,4 @@ class TestAPIOnlyTests:
     def test_firebug_version_number(self, mozwebqa):
         """Testcase for Litmus 15317"""
         addon_xml = AddOnsAPI(mozwebqa)
-        Assert.equal("1.8.0", addon_xml.get_addon_version_number("Firebug"))
+        Assert.equal("1.8.1", addon_xml.get_addon_version_number("Firebug"))
