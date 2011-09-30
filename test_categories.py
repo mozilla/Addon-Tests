8d1269284c16ace94ee17a62e56c1d793e515210
Geo Mealer <geo@snarksoft.com>
2011-09-29 19:05:19 -0700

    Added name to contributor list

--- test_categories.py
+++ test_categories.py
@@ -22,6 +22,7 @@
 #
 # Contributor(s): Marlena Compton <mcompton@mozilla.com>
 #                 Bebe
+#                 Geo Mealer <gmealer@mozilla.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
