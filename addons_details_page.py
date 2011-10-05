5fa4f16af335ca806470962592f64fb81c6f8811
bebef1987 <florin.strugariu@softvision.ro>
2011-10-05 14:15:47 +0300

    fixed addon name

--- addons_details_page.py
+++ addons_details_page.py
@@ -106,7 +106,8 @@ class AddonsDetailsPage(AddonsBasePage):
         #formats name for url
         AddonsBasePage.__init__(self, testsetup)
         if (addon_name != None):
-            self.addon_name = re.sub(r'[^\w-]', '', addon_name).lower()
+            self.addon_name = addon_name.replace(" ", "-")
+            self.addon_name = re.sub(r'[^A-Za-z0-9\-]', '', self.addon_name).lower()
             self.addon_name = self.addon_name[:27]
             self.selenium.open("%s/addon/%s" % (self.site_version, self.addon_name))
             self._wait_for_reviews_and_other_addons_by_author_to_load()
