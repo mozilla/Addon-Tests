01b82424c77318948d4f5d01db6078be1738c214
Alin <trifandreialin@gmail.com>
2012-01-18 16:18:24 +0200

    Updated

--- pages/discovery.py
+++ pages/discovery.py
@@ -50,7 +50,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from pages.page import Page
 from pages.base import Base
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class DiscoveryPane(Base):
