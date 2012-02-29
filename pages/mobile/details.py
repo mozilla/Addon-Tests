65587418147077b3465a6f0c03a904b45e058ac0
Teodosia <teodosia.pop@gmail.com>
2012-02-29 15:15:06 +0200

    Check that contribute button doesn't show up on mobile details page. https://www.pivotaltracker.com/story/show/24955445 STR: 1. From the mobile site, go to the details page for Adblock Plus. 2. In the details page, make sure we don't show Contribute button.

---
+++ pages/mobile/details.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import re
+
+from selenium.webdriver.common.by import By
+
+from pages.mobile.base import Base
+
+
+class Details(Base):
+
+    _title_locator = (By.CSS_SELECTOR, 'div.infobox > h3')
+    _buttons_locator = (By.CSS_SELECTOR, '.button')
+
+    def __init__(self, testsetup, addon_name=None):
+        #formats name for url
+        Base.__init__(self, testsetup)
+        if (addon_name != None):
+            self.addon_name = addon_name.replace(" ", "-")
+            self.addon_name = re.sub(r'[^A-Za-z0-9\-]', '', self.addon_name).lower()
+            self.addon_name = self.addon_name[:27]
+            self.selenium.get("%s/addon/%s" % (self.base_url, self.addon_name))
+
+    @property
+    def _page_title(self):
+        return "%s :: Add-ons for Firefox" % self.title
+
+    @property
+    def title(self):
+        return self.selenium.find_element(*self._title_locator).text
+
+    @property
+    def buttons(self):
+        return [element.text for element in self.selenium.find_elements(*self._buttons_locator)]
