7a41a308823b36a133e02ffb425653d5d2534ef8
Marlena Compton <mozmarlena@gmail.com>
2012-03-07 11:31:28 -0800

    fixed bug number

--- tests/desktop/test_api_only.py
+++ tests/desktop/test_api_only.py
@@ -17,7 +17,7 @@ xfail = pytest.mark.xfail
 @pytest.mark.skip_selenium
 class TestAPIOnlyTests:
 
-    @xfail(reason="bug 708970")
+    @xfail(reason="bug 733626")
     def test_that_firebug_is_listed_first_in_addons_search_for_fire(self, mozwebqa):
         """Test for Litmus 15314."""
         addons_xml = AddOnsAPI(mozwebqa, 'fire')
