8a0bb84527d9c3237d2c61e7421fa9c639c180e2
Alin <trifandreialin@gmail.com>
2012-03-08 12:00:52 +0200

    Refactor name of mobile test

--- tests/mobile/test_home.py
+++ tests/mobile/test_home.py
@@ -26,7 +26,7 @@ class TestHome:
         Assert.true(home_desktop.is_the_current_page)
 
     @pytest.mark.nondestructive
-    def test_that_checks_the_header_menu(self, mozwebqa):
+    def test_that_checks_header_text_and_page_title(self, mozwebqa):
         home = Home(mozwebqa)
         Assert.true(home.is_the_current_page)
 
