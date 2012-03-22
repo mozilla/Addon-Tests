f7927eb9b78ad2009b58554e2a87862ee2a360af
Leah Klearman <lklrmn@gmail.com>
2012-03-22 16:46:43 -0700

    tests for install theme and install jetpack

---
+++ tests/desktop/test_installs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import pytest
+
+from unittestzero import Assert
+
+from pages.desktop.home import Home
+
+
+class TestInstalls:
+
+    @pytest.mark.nondestructive
+    def test_could_install_theme(self, mozwebqa):
+        """
+        Test for Litmus 15115
+        https://litmus.mozilla.org/show_test.cgi?id=15115
+        note that this test does not actually *install* the theme
+        """
+
+        home_page = Home(mozwebqa)
+        themes_page = home_page.header.site_navigation_menu("Themes").click()
+        theme_page = themes_page.click_on_first_addon()
+        Assert.true(theme_page.install_button_exists)
+        
+        
+    @pytest.mark.nondestructive
+    def test_could_install_jetpack(self, mozwebqa):
+        """
+        Test for Litmus 17355
+        https://litmus.mozilla.org/show_test.cgi?id=17355
+        note that this test does not actually *install* the jetpack
+        """
+        
+        home_page = Home(mozwebqa)
+        search_page = home_page.header.search_for("jetpack")
+        details_page = search_page.results.pop().click_result()
+        Assert.true(details_page.is_version_information_install_button_visible)
+
