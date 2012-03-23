b6c357cb7fe2e023070a917f086eabc892e861ff
Leah Klearman <lklrmn@gmail.com>
2012-03-23 08:52:35 -0700

    remove some whitespace on blank lines

--- tests/desktop/test_installs.py
+++ tests/desktop/test_installs.py
@@ -23,15 +23,14 @@ class TestInstalls:
         themes_page = home_page.header.site_navigation_menu("Themes").click()
         theme_page = themes_page.click_on_first_addon()
         Assert.true(theme_page.install_button_exists)
-        
-        
+
+
     @pytest.mark.nondestructive
     @pytest.mark.litmus(17355)
     def test_could_install_jetpack(self, mozwebqa):
         """note that this test does not actually *install* the jetpack"""
-        
+
         home_page = Home(mozwebqa)
         search_page = home_page.header.search_for("jetpack")
         details_page = search_page.results.pop().click_result()
         Assert.true(details_page.is_version_information_install_button_visible)
-
