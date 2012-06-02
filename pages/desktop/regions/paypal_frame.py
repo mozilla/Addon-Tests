7992124f84eddff28b595d08d208a6bf5fff1103
Dave Hunt <dave.hunt@gmail.com>
2012-06-02 01:20:11 +0100

    Add an explicit wait when initialising the Paypal frame region to be confident that the contents has loaded.

--- pages/desktop/regions/paypal_frame.py
+++ pages/desktop/regions/paypal_frame.py
@@ -8,16 +8,22 @@
 from pages.page import Page
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class PayPalFrame(Page):
 
     _iframe_id = 'PPDGFrame'
+    _logo_locator = (By.CSS_SELECTOR, '#launcher .logo img')
     _paypal_login_button = (By.CSS_SELECTOR, 'div.logincnt > p > a.button')
 
     def __init__(self, testsetup):
         Page.__init__(self, testsetup)
         self.selenium.switch_to_frame(self._iframe_id)
+        # wait for the paypal logo to appear, then we know the frame's contents has loaded
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: s.find_element(*self._logo_locator),
+            'Timeout waiting for Paypal logo in frame.')
 
     def login_to_paypal(self, user="paypal"):
         self.selenium.find_element(*self._paypal_login_button).click()
