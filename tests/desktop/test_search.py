ea5328c576f16e5e6150dd61b417b5c6e760e5c0
Stephen Donner <stephen.donner@gmail.com>
2012-12-14 12:47:39 -0800

    removing xfails for sorting, from bug 819910

--- tests/desktop/test_search.py
+++ tests/desktop/test_search.py
@@ -147,7 +147,6 @@ class TestSearch:
 
     @pytest.mark.native
     @pytest.mark.nondestructive
-    @pytest.mark.xfail(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=819911 - [dev]Sorting by weekly downloads is broken')
     def test_sorting_by_downloads(self, mozwebqa):
         """
         Test for Litmus 17342.
@@ -193,8 +192,7 @@ class TestSearch:
 
     @pytest.mark.native
     @pytest.mark.nondestructive
-    @pytest.mark.xfail(reason='https://bugzilla.mozilla.org/show_bug.cgi?id=819910 - [dev] Sorting by most users is broken')
-    def test_sorting_by_number_of_most_users(self, mozwebqa):
+        def test_sorting_by_number_of_most_users(self, mozwebqa):
         """
         Test for Litmus 24867.
         https://litmus.mozilla.org/show_test.cgi?id=24867
