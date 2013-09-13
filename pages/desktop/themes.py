447afa19721dbeea5bf6256cea4b75c6c31231af
roarange <roarange3@gmail.com>
2013-09-13 19:59:17 -0400

    added WebDriverWait(...).isdisplayed() to wait for theme locator visibility

--- pages/desktop/themes.py
+++ pages/desktop/themes.py
@@ -35,8 +35,11 @@ class Themes(Base):
 
     def click_theme(self, index):
         """Clicks on the theme with the given index in the page."""
+        WebDriverWait(self.selenium, self.timeout).until(lambda s: self.selenium.find_elements(*self._themes_locator)[index].is_displayed())
+ 
         self.selenium.find_elements(*self._themes_locator)[index].click()
         theme_detail = ThemesDetail(self.testsetup)
+
         WebDriverWait(self.selenium, self.timeout).until(lambda s: theme_detail.is_title_visible)
         return theme_detail
 
